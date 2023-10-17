6a91fa79564333392ede1b794417b4aa18018e07
Xceptions <agbokenechukwu.k@gmail.com>
2023-10-17 17:59:26 +0100

    added react hook to change variable

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 function ListGroup() {
+  let items = ["Enugu", "Anambra", "Imo", "Ebonyi", "Abia"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>Header</h1>
+      {items.length == 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
